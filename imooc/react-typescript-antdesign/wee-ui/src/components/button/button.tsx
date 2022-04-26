bedbdc38565224b1f6ec6022b70a437f1bc6a334
weeshin <weeshin@foxmail.com>
2022-04-26 12:46:45 +0800

    update: eslint config file

---
+++ imooc/react-typescript-antdesign/wee-ui/src/components/button/button.tsx
@@ -0,0 +1,18 @@
+export enum ButtonSize {
+  large = 'large',
+  small = 'small',
+}
+
+export enum ButtonType {
+  primary = 'primay',
+  info = 'info',
+  success = 'success',
+  alert = 'alert',
+  error = 'error',
+}
+const a = '123';
+if (a == '123') {
+  console.log(123);
+}
+
+let b;
