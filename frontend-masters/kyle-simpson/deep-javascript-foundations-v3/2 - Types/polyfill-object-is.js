973bc7785f2506e440937f76b74e8af6cbebd1f5
weeshin <weeshin@foxmail.com>
2022-04-30 09:24:32 +0800

    add: exercise files of frontend-masters/deep-javascript-foundations

---
+++ frontend-masters/kyle-simpson/deep-javascript-foundations-v3/2 - Types/polyfill-object-is.js
@@ -0,0 +1,40 @@
+if (!Object.is || true) {
+  Object.is = function ObjectIs(x, y) {
+    var xNegZero = isNegZero(x);
+    var yNegZero = isNegZero(y);
+    if (xNegZero || yNegZero) {
+      return xNegZero && yNegZero;
+    } else if (isNaN(x) && isNaN(y)) {
+      return true;
+    } else {
+      return x === y;
+    }
+
+    function isNegZero(v) {
+      return v == 0 && 1 / v == -Infinity;
+    }
+
+    function isNaN(v) {
+      return v !== v;
+    }
+  };
+}
+
+console.log(Object.is(42, 42) === true);
+console.log(Object.is('foo', 'foo') === true);
+console.log(Object.is(false, false) === true);
+console.log(Object.is(null, null) === true);
+console.log(Object.is(undefined, undefined) === true);
+console.log(Object.is(NaN, NaN) === true);
+console.log(Object.is(-0, -0) === true);
+console.log(Object.is(0, 0) === true);
+
+console.log(Object.is(-0, 0) === false);
+console.log(Object.is(0, -0) === false);
+console.log(Object.is(0, NaN) === false);
+console.log(Object.is(NaN, 0) === false);
+console.log(Object.is(42, '42') === false);
+console.log(Object.is('42', 42) === false);
+console.log(Object.is(false, true) === false);
+console.log(Object.is(null, undefined) === false);
+console.log(Object.is(undefined, null) === false);
