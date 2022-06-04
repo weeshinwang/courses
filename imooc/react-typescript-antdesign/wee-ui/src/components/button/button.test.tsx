48fae3f1d70bf62c8d2d02e23f3a673255d5eb99
weeshin <weeshin@foxmail.com>
2022-06-04 23:14:23 +0800

    fix: eslint configs for the course imooc/react-typescript-antdesign

---
+++ imooc/react-typescript-antdesign/wee-ui/src/components/button/button.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Button from './button';
+
+test('OUR FIRST RENDER', () => {
+  render(<Button>Nice</Button>);
+  const element = screen.queryByText('Nice');
+  expect(element).toBeTruthy();
+});
