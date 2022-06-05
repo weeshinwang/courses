c212e352603e56938f0b560b2317ce244c67520c
weeshin <weeshin@foxmail.com>
2022-06-05 15:09:01 +0800

    add: codes of chapter6-menu for imooc/react-typescript-antdesign

--- imooc/react-typescript-antdesign/wee-ui/src/App.tsx
+++ imooc/react-typescript-antdesign/wee-ui/src/App.tsx
@@ -1,20 +1,33 @@
 import Button from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Button>DEFAULT</Button>
-        <Button disabled>DISABLED</Button>
-        <Button btnType='primary' size='lg'>
-          PRIMARY
-        </Button>
-        <Button btnType='danger' size='sm'>
-          PRIMARY
-        </Button>
-        <Button btnType='link' target='_blank' href='https://www.baidu.com'>
-          LINK
-        </Button>
+        <div className='menu'>
+          <Menu defaultIndex='0' onSelect={(i) => alert(i)}>
+            <MenuItem index='1'>COOL LINK1</MenuItem>
+            <MenuItem index='2' disabled>
+              COOL LINK2
+            </MenuItem>
+            <MenuItem index='3'>COOL LINK3</MenuItem>
+          </Menu>
+        </div>
+        <div className='button'>
+          <Button>DEFAULT</Button>
+          <Button disabled>DISABLED</Button>
+          <Button btnType='primary' size='lg'>
+            PRIMARY
+          </Button>
+          <Button btnType='danger' size='sm'>
+            PRIMARY
+          </Button>
+          <Button btnType='link' target='_blank' href='https://www.baidu.com'>
+            LINK
+          </Button>
+        </div>
       </header>
     </div>
   );
---
+++ imooc/react-typescript-antdesign/wee-ui/src/components/Menu/menu.tsx
@@ -0,0 +1,88 @@
+import React, { FC, useState, createContext, CSSProperties } from 'react';
+import classNames from 'classnames';
+import { MenuItemProps } from './menuItem';
+
+type MenuMode = 'horizontal' | 'vertical';
+
+export interface MenuProps {
+  defaultIndex?: string;
+  className?: string;
+  mode?: MenuMode;
+  style?: CSSProperties;
+  onSelect?: (selectedIndex: string) => void;
+  defaultOpenSubMenus?: string[];
+  children?: React.ReactNode;
+}
+
+interface IMenuContext {
+  index: string;
+  onSelect?: (selectedIndex: string) => void;
+  mode?: MenuMode;
+  defaultOpenSubMenus?: string[];
+}
+
+export const MenuContext = createContext<IMenuContext>({ index: '0' });
+
+const Menu: FC<MenuProps> = (props) => {
+  const {
+    className,
+    mode,
+    style,
+    children,
+    defaultIndex,
+    onSelect,
+    defaultOpenSubMenus,
+  } = props;
+
+  const [currentActive, setActive] = useState(defaultIndex);
+  const classes = classNames('viking-menu', className, {
+    'menu-vertical': mode === 'vertical',
+    'menu-horizontal': mode !== 'vertical',
+  });
+
+  const handleClick = (index: string) => {
+    setActive(index);
+    if (onSelect) {
+      onSelect(index);
+    }
+  };
+
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : '0',
+    onSelect: handleClick,
+    mode,
+    defaultOpenSubMenus,
+  };
+
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement =
+        child as React.FunctionComponentElement<MenuItemProps>;
+      const { displayName } = childElement.type;
+      if (displayName === 'MenuItem' || displayName === 'SubMenu') {
+        return React.cloneElement(childElement, {
+          index: index.toString(),
+        });
+      } else {
+        console.error(
+          'Warning: Menu has a child which is not a MenuItem component'
+        );
+      }
+    });
+  };
+  return (
+    <ul className={classes} style={style} data-testid='test-menu'>
+      <MenuContext.Provider value={passedContext}>
+        {renderChildren()}
+      </MenuContext.Provider>
+    </ul>
+  );
+};
+
+Menu.defaultProps = {
+  defaultIndex: '0',
+  mode: 'horizontal',
+  defaultOpenSubMenus: [],
+};
+
+export default Menu;
